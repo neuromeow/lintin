68e414d522c0adfd6cf0d5ff0b644f7a57a54d24
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-11-14 19:40:32 +0300

    feat: add the ability to read the lines of from stdin

--- src/core.rs
+++ src/core.rs
@@ -1,10 +1,29 @@
-use clap::Parser;
+use clap::{CommandFactory, Parser};
+use is_terminal::IsTerminal as _;
 use std::error::Error;
+use std::{
+    fs::File,
+    io::{stdin, BufReader},
+    path::PathBuf,
+};
 
 use crate::cli::Cli;
+use crate::util;
 
 pub fn run() -> Result<(), Box<dyn Error>> {
     let args = Cli::parse();
-    println!("{:?}", args.file);
+    let file = args.file;
+    let lines_from_file = if *file == PathBuf::from("-") {
+        if stdin().is_terminal() {
+            Cli::command().print_help().unwrap();
+            std::process::exit(2);
+        }
+        util::read_lines_from_buf_reader(BufReader::new(stdin().lock()))
+    } else {
+        util::read_lines_from_buf_reader(BufReader::new(File::open(file).unwrap()))
+    };
+    for line in lines_from_file {
+        println!("{}", line);
+    }
     Ok(())
 }
--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,5 @@
 use std::io::BufRead;
 
-#[allow(dead_code)]
 pub fn read_lines_from_buf_reader<R: BufRead>(buf_reader: R) -> Vec<String> {
     buf_reader.lines().map(|line| line.unwrap()).collect()
 }
