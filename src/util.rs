a311efde9a26eefb94cf02a78eaa083d04798c0a
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-11-15 20:35:40 +0300

    feat: add function to read file using BufReader

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,14 @@
-use std::io::BufRead;
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
+
+#[allow(dead_code)]
+pub fn create_file_bufreader(file_pathname: &PathBuf) -> Result<BufReader<File>, Box<dyn Error>> {
+    let file = File::open(file_pathname)?;
+    let file_bufreader = BufReader::new(file);
+    Ok(file_bufreader)
+}
 
 pub fn read_lines_from_buf_reader<R: BufRead>(buf_reader: R) -> Vec<String> {
     buf_reader.lines().map(|line| line.unwrap()).collect()
