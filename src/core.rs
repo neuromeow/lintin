fe1d9044e6dd71fd0d83d470f38058e8f4d1ddb9
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-11-15 20:10:10 +0300

    feat: separate logic for single and multiple arguments

--- src/core.rs
+++ src/core.rs
@@ -12,18 +12,24 @@ use crate::util;
 
 pub fn run() -> Result<(), Box<dyn Error>> {
     let args = Cli::parse();
-    let file = args.file;
-    let lines_from_file = if *file == PathBuf::from("-") {
-        if stdin().is_terminal() {
-            Cli::command().print_help().unwrap();
-            std::process::exit(2);
+    let files_to_check = args.file_or_dir;
+    // Possible scenario that the argument list is empty (the number of arguments is 0) is checked by clap
+    if files_to_check.len() == 1 {
+        let file_to_check = files_to_check.get(0).unwrap();
+        let lines_from_file = if *file_to_check == PathBuf::from("-") {
+            if stdin().is_terminal() {
+                Cli::command().print_help().unwrap();
+                std::process::exit(2);
+            }
+            util::read_lines_from_buf_reader(BufReader::new(stdin().lock()))
+        } else {
+            util::read_lines_from_buf_reader(BufReader::new(File::open(file_to_check).unwrap()))
+        };
+        for line in lines_from_file {
+            println!("{}", line);
         }
-        util::read_lines_from_buf_reader(BufReader::new(stdin().lock()))
     } else {
-        util::read_lines_from_buf_reader(BufReader::new(File::open(file).unwrap()))
-    };
-    for line in lines_from_file {
-        println!("{}", line);
+        println!("Several arguments are present.");
     }
     Ok(())
 }
