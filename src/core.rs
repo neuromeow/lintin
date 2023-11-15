07961b8504f182ead840bb7586377bc5c57c6d86
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-11-15 20:55:49 +0300

    feat: add use of a function to recursively traverse directories

--- src/core.rs
+++ src/core.rs
@@ -4,7 +4,7 @@ use is_terminal::IsTerminal as _;
 use std::error::Error;
 // use std::path::Path;
 use std::{
-    fs::File,
+    // fs::File,
     io::{stdin, BufReader},
     path::PathBuf,
 };
@@ -16,7 +16,6 @@ fn parse_lines_from_file(_lines_from_file: Vec<String>) {
     println!("Lines parsed.");
 }
 
-#[allow(dead_code)]
 fn walk_to_find_files(
     file_or_dir_pathname: &PathBuf,
     files_to_check: &mut Vec<PathBuf>,
@@ -39,12 +38,13 @@ fn walk_to_find_files(
 
 pub fn run() -> Result<(), Box<dyn Error>> {
     let args = Cli::parse();
-    let files_to_check = args.file_or_dir;
+    // It could also just be `-` to start the process of reading lines from stdin.
+    let files_or_dirs_to_check = args.file_or_dir;
     // Possible scenario that the argument list is empty (the number of arguments is 0) is checked by `clap`.
-    if files_to_check.len() == 1 {
+    if files_or_dirs_to_check.len() == 1 {
         // `unwrap` never returns `panic` for the reason above.
-        let file_to_check = files_to_check.get(0).unwrap();
-        let lines_from_file = if *file_to_check == PathBuf::from("-") {
+        let file_or_dir_to_check = files_or_dirs_to_check.get(0).unwrap();
+        if *file_or_dir_to_check == PathBuf::from("-") {
             // It requires that stdin is not interactive because we’re expecting input
             // that’s piped through to the program, not text that’s typed in at runtime.
             // If stdin is a tty, it outputs the help docs so that it’s clear why it doesn't work.
@@ -54,17 +54,26 @@ pub fn run() -> Result<(), Box<dyn Error>> {
                 std::process::exit(2);
             }
             println!("stdin");
-            util::read_lines_from_buf_reader(BufReader::new(stdin().lock()))
+            let lines_from_stdin = util::read_lines_from_buf_reader(BufReader::new(stdin().lock()));
+            parse_lines_from_file(lines_from_stdin);
+            println!();
         } else {
-            println!("{:?}", file_to_check);
-            util::read_lines_from_buf_reader(BufReader::new(File::open(file_to_check).unwrap()))
-        };
-        parse_lines_from_file(lines_from_file);
-        println!();
+            // println!("{:?}", file_or_dir_to_check);
+            let mut files_to_check: Vec<PathBuf> = Vec::new();
+            walk_to_find_files(file_or_dir_to_check, &mut files_to_check)?;
+            // println!("{:?}", files_to_check);
+            for file_to_check_pathname in files_to_check {
+                println!("{}", file_to_check_pathname.display());
+                let file_bufreader = util::create_file_bufreader(&file_to_check_pathname)?;
+                let lines_from_file = util::read_lines_from_buf_reader(file_bufreader);
+                parse_lines_from_file(lines_from_file);
+                println!();
+            }
+        }
     } else {
-        for file_to_check in &files_to_check {
+        for file_or_dir_to_check in &files_or_dirs_to_check {
             // The `-` argument to read lines from stdin must not be allowed along with other arguments.
-            if *file_to_check == PathBuf::from("-") {
+            if *file_or_dir_to_check == PathBuf::from("-") {
                 Cli::command()
                     .error(
                         ErrorKind::ArgumentConflict,
@@ -73,13 +82,16 @@ pub fn run() -> Result<(), Box<dyn Error>> {
                     .exit();
             }
         }
-        for file_to_check in files_to_check {
-            println!("{:?}", file_to_check);
-            let lines_from_file = util::read_lines_from_buf_reader(BufReader::new(
-                File::open(file_to_check.clone()).unwrap(),
-            ));
+        let mut files_to_check: Vec<PathBuf> = Vec::new();
+        for file_or_dir_to_check in &files_or_dirs_to_check {
+            walk_to_find_files(file_or_dir_to_check, &mut files_to_check)?;
+        }
+        for file_to_check_pathname in files_to_check {
+            println!("{}", file_to_check_pathname.display());
+            let file_bufreader = util::create_file_bufreader(&file_to_check_pathname)?;
+            let lines_from_file = util::read_lines_from_buf_reader(file_bufreader);
             parse_lines_from_file(lines_from_file);
-            println!();
+            println!()
         }
     }
     Ok(())
