652915a39c0a620a8370ddcd5a8f1b6058821ba7
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-11-20 19:15:11 +0300

    test: add unit tests for the inventory validation function

--- src/inventory_validator.rs
+++ src/inventory_validator.rs
@@ -16,3 +16,25 @@ pub fn validate_inventory<R: BufRead>(reader: R) -> Vec<String> {
     }
     found_errors
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Cursor;
+
+    #[test]
+    fn test_validate_inventory_reading_errors_do_not_exist() {
+        let cursor = Cursor::new(b"Valid line\n");
+        let found_errors = validate_inventory(cursor);
+        assert!(found_errors.is_empty());
+    }
+
+    #[test]
+    fn test_validate_inventory_reading_errors_exist() {
+        let corrupt_data: &[u8] = &[0, 159, 146, 150];
+        let cursor = Cursor::new(corrupt_data);
+        let result = validate_inventory(cursor);
+        assert_eq!(result.len(), 1);
+        assert!(result[0].starts_with("Error reading line 1:"));
+    }
+}
