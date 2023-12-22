403250b8dea0d07262aa8c7f49a13529c33b91b4
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-12-22 17:10:32 +0300

    test: update unit tests for the inventory validation function

--- src/inventory_validator.rs
+++ src/inventory_validator.rs
@@ -22,19 +22,19 @@ mod tests {
     use std::io::Cursor;
 
     #[test]
-    fn test_validate_inventory_reading_errors_do_not_exist() {
-        let cursor = Cursor::new(b"Valid line\n");
-        let found_errors = validate_inventory(cursor);
-        assert!(found_errors.is_empty());
-
+    fn test_validate_inventory_line_reading_errors_do_not_exist() {
+        let valid_data = b"Valid line\n";
+        let cursor = Cursor::new(valid_data);
+        let inventory_errors = validate_inventory(cursor);
+        assert!(inventory_errors.is_empty());
     }
 
     #[test]
-    fn test_validate_inventory_reading_errors_exist() {
-        let corrupt_data: &[u8] = &[0, 159, 146, 150];
-        let cursor = Cursor::new(corrupt_data);
-        let result = validate_inventory(cursor);
-        assert_eq!(result.len(), 1);
-        assert!(result[0].starts_with("Error reading line 1:"));
+    fn test_validate_inventory_line_reading_errors_exist() {
+        let corrupted_data = &[0, 159, 146, 150];
+        let cursor = Cursor::new(corrupted_data);
+        let inventory_errors = validate_inventory(cursor);
+        assert_eq!(inventory_errors.len(), 1);
+        assert!(inventory_errors[0].starts_with("1 Line reading error:"));
     }
 }
